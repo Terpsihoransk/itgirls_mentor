804ff4367991ba6c8fe77ffe2a5c9e4ae0755f55
TerpsihoraNSK <arsenjeva.ea@gmail.com>
2023-02-17 06:49:53 +0700

    Initial commit

---
+++ src/main/java/ru/itgirls/itgirls/ItgirlsApplication.java
@@ -0,0 +1,13 @@
+package ru.itgirls.itgirls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItgirlsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItgirlsApplication.class, args);
+    }
+
+}
